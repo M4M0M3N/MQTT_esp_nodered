a73f5e828de50027b2bfbd8d01c1fa3c6a2e28b0
M4M0M3N <79576670+M4M0M3N@users.noreply.github.com>
2022-02-17 16:29:04 +0100

    Script publisher
    Questo script, una volta inserite el proprie informazioni, invia un messaggio con topic allarme, quando viene messo a massa il pin 0. Basta inserire un semplice pulsante normalmente aperto tra il pin 0 e il pin gnd.
    La pressione del pulsante si puo' notare andando a leggere il serial monitor.
    Il contenuto del messaggio si puo' andare a vedere registrandosi al topic allarme, il payload e' true

---
+++ esp_mqtt_pub/esp_mqtt_pub.ino
@@ -0,0 +1,43 @@
+#include "EspMQTTClient.h"
+
+EspMQTTClient client(
+  "SSID wify",
+  "PASS wify",
+  "IP broker",               // MQTT Broker server ip
+  "MQTTUsername",            // Can be omitted if not needed
+  "MQTTPassword",            // Can be omitted if not needed
+  "NAME esp in mqtt",        // Client name that uniquely identify your device
+  1883                       // The MQTT port, default to 1883. this line can be omitted
+);
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(0, INPUT);
+
+  // Optional functionalities of EspMQTTClient
+  //client.enableDebuggingMessages(); // Enable debugging messages sent to serial output
+  //client.enableHTTPWebUpdater(); // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
+  //client.enableOTA(); // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
+  //client.enableLastWillMessage("TestClient/lastwill", "I am going offline");  // You can activate the retain flag by setting the third parameter to true
+}
+
+void onConnectionEstablished()
+{
+
+}
+
+
+void loop()
+{ 
+  if ( !digitalRead(0) ){
+    Serial.println("Allarme");
+    client.publish("allarme","true");
+
+    while ( !digitalRead(0) )
+    { /*aspetto che viene rilasciato il pulsante*/ }
+  }
+  
+  
+  client.loop();
+}
